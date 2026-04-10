ff69f7d0512c7a852699ba31fe6022f57b416768
P.YASHASWINI@1224 <pyashaswinipyashaswini@gmail.com>
2026-04-10 09:08:48 +0530

    Add files via upload

---
+++ salary_calculation.py
@@ -0,0 +1,21 @@
+# Salary Calculation Model
+
+# Input values
+basic_salary = 20000
+
+# Allowances
+hra = 0.20 * basic_salary
+da = 0.10 * basic_salary
+
+# Deduction
+tax = 0.05 * basic_salary
+
+# Net Salary Calculation
+net_salary = basic_salary + hra + da - tax
+
+# Output
+print("Basic Salary:", basic_salary)
+print("HRA:", hra)
+print("DA:", da)
+print("Tax:", tax)
+print("Net Salary:", net_salary)
